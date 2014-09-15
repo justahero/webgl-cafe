c2b0daa089780c51ca48f0d68e30c7e3e6383d48
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-09-15 23:36:41 +0200

    Removes unnecessary lines and comments from config

--- config.rb
+++ config.rb
@@ -1,25 +1,15 @@
-require 'dotenv'
-
 TARGET = ENV['TARGET'] || environment
 
 # Livereload
 activate :livereload
-config[:file_watcher_ignore] += [ /.idea\// ]
-
 activate :directory_indexes
 
-set :css_dir,     'stylesheets'
-set :js_dir,      'javascripts'
-set :images_dir,  'images'
+set :css_dir,    'stylesheets'
+set :js_dir,     'javascripts'
+set :images_dir, 'images'
 
-# If you need additional libraries or styles
-# just drop in libs/javascripts and insert require in all.js.coffee
-sprockets.append_path  "libs/javascripts"
-# sprockets.append_path  "libs/stylesheets"
-# sprockets.append_path  "libs/fonts"
-# sprockets.append_path  "libs/images"
+sprockets.append_path "libs/javascripts"
 sprockets.append_path "vendor"
-
 
 configure :development do
   set :debug_assets, true
