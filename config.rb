463d3ad26760d0f81d9caa78f9791337da087559
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2014-09-28 19:07:55 +0200

    Checks samples folder and uses proxy for generating files
    Currently not working!

--- config.rb
+++ config.rb
@@ -12,6 +12,13 @@ sprockets.append_path "libs/javascripts"
 sprockets.append_path "resources"
 sprockets.append_path "vendor"
 
+# auto generate all Cafe sample files
+samples = Dir['source/samples/*/'].map{ |d| File.basename(d) }
+samples.each do |sample|
+  STDOUT.puts "SAMPLE: #{sample}"
+  proxy "examples/#{sample}/index.html", "sample.html", locals: { :sample => sample }, :ignore => true
+end
+
 configure :development do
   set :debug_assets, true
 end
